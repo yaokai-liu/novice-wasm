e249127884552132d75549c41714bbbcf20c0312
Yaokai Liu <yaokai.liu@foxmail.com>
2024-06-14 15:55:37 +0800

    update.

--- tests/quickSort.test.ts
+++ tests/quickSort.test.ts
@@ -20,27 +20,30 @@ suite("testQuickSort", () => {
         const array1: Array<number> = [33, 23, 2, 1, 9, 12, 5, 8, 2, 3, 1, 4, 6, 7,10];
         const array2: Array<number> = [33, 23, 2, 1, 9, 12, 5, 8, 2, 3, 1, 4, 6, 7,10];
         const compareFn = (a:number, b:number) => { return a == b  ? 0 : a > b ? 1 : -1; };
-        setArray(array1, 0);
+        const startAddress = 0;
+        setArray(array1, startAddress);
         array2.sort(compareFn);
-        NoviceWasm.quickSort32(0, array1.length);
+        NoviceWasm.quickSort32(startAddress, array1.length);
         check(array2, 0, array1.length);
     });
     test("testQuickSortReversed", () => {
         const array1: Array<number> = [9, 8, 7, 6, 5, 4, 3, 2, 1];
         const array2: Array<number> = [9, 8, 7, 6, 5, 4, 3, 2, 1];
         const compareFn = (a:number, b:number) => { return a == b  ? 0 : a > b ? 1 : -1; };
-        setArray(array1, 0);
+        const startAddress = 0;
+        setArray(array1, startAddress);
         array2.sort(compareFn);
-        NoviceWasm.quickSort32(0, array1.length);
+        NoviceWasm.quickSort32(startAddress, array1.length);
         check(array2, 0, array1.length);
     });
     test("testQuickSortOdered", () => {
         const array1: Array<number> = [3, 5, 6, 8, 9, 10, 21, 33, 45, 69];
         const array2: Array<number> = [3, 5, 6, 8, 9, 10, 21, 33, 45, 69];
         const compareFn = (a:number, b:number) => { return a == b  ? 0 : a > b ? 1 : -1; };
-        setArray(array1, 0);
+        const startAddress = 0;
+        setArray(array1, startAddress);
         array2.sort(compareFn);
-        NoviceWasm.quickSort32(0, array1.length);
+        NoviceWasm.quickSort32(startAddress, array1.length);
         check(array2, 0, array1.length);
     });
   });
--- watsrc/novice.wat
+++ watsrc/novice.wat
@@ -1,6 +1,5 @@
 (module
     (import "js" "mem" (memory 1))
-    (type $t_cmp_fn (func (param $_ele1 i32) (param $_ele2 i32) (result i32)))
     (func $splat64 (param $a i64) (result i64)  (local $_debug i64)
         (i64.shr_u  (i64.shl (local.get  $a) (i64.const  56)) (i64.const  56))
         (i64.or (i64.shl (local.tee $a) (i64.const 8 )) (local.get $a))
